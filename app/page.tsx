3a7c96441437353316ac4f225098ee4dc8891ca7
101ta28 <workta28@gmail.com>
2025-02-10 22:56:42 +0900

    トップページの表記を変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 import { Link } from "@heroui/link";
-import { Snippet } from "@heroui/snippet";
-import { Code } from "@heroui/code";
 import { button as buttonStyles } from "@heroui/theme";
 
 import { siteConfig } from "@/config/site";
@@ -23,14 +21,6 @@ export default function Home() {
           <GithubIcon size={20} />
           GitHub
         </Link>
-      </div>
-
-      <div className="mt-8">
-        <Snippet hideCopyButton hideSymbol variant="bordered">
-          <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
-          </span>
-        </Snippet>
       </div>
     </section>
   );
